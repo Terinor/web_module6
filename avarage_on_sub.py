f901689b0b01f49535154d582eda370392bf650d
Terinor <kirstruk01@gmail.com>
2024-01-02 09:23:09 +0200

    few_new_request

--- avarage_on_sub.py
+++ avarage_on_sub.py
@@ -1,4 +1,4 @@
-#2 Знайти студента із найвищим середнім балом з певного предмета:
+#3 Знайти середній бал у групах з певного предмета:
 
 import asyncpg
 import asyncio
@@ -18,19 +18,21 @@ async def main(user, password, database, host):
     conn = await asyncpg.connect(user=user, password=password, database=database, host=host)
 
     query = """
-        SELECT gr.group_id, AVG(g.grade) AS average_grade
+        SELECT gr.group_name, sub.name AS subject_name, AVG(g.grade) AS average_grade
         FROM Groups gr
         JOIN Students s ON gr.group_id = s.group_id
         JOIN Grades g ON s.student_id = g.student_id
+        JOIN Subjects sub ON g.subject_id = sub.subject_id
         WHERE g.subject_id = 3
-        GROUP BY gr.group_id;
+        GROUP BY gr.group_name, sub.name;
+
 
     """
     students_top5 = await execute_query(conn, query)
-    print(f"Top student with the highest average grades on subject: ")
+    print(f"Groups with average grades on subject: ")
     for record in students_top5:
-        print(record)
-        # print(f"{record['subject_name']}: {record['name']}: {record['average_grade']:.2f}")
+        #print(record)
+        print(f"subject {record['subject_name']}: {record['group_name']}: {record['average_grade']:.2f}")
 
     await conn.close()
 
